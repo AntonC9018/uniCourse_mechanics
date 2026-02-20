e5452be1721d9d2b7919ab96fedc5acce553d908
AntonC9018 <anton.curm@gmail.com>
2026-02-20 09:22:14 +0200

    Unit test

--- src/02_grid/Assets/_project/Code/Core.Tests/GridTests.cs
+++ src/02_grid/Assets/_project/Code/Core.Tests/GridTests.cs
@@ -1,12 +1,17 @@
 using NUnit.Framework;
+using UnityEngine;
 
 namespace Core.Tests
 {
     public sealed class GridTests
     {
         [Test]
-        public void Test()
+        public void WorldToGrid_BackAndForth_Works()
         {
+            var grid = new GridSize(5, 5);
+            var gridPosition = GridHelper.WorldToGridPosition(grid, new(4, 3));
+            var worldPosition = GridHelper.GridToWorldPosition(grid, gridPosition);
+            Assert.AreEqual(new Vector2(4, 3), worldPosition);
         }
     }
 }
