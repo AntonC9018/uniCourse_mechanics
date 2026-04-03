21f194e47c33961be7fe8bcfa946345651b481c3
AntonC9018 <anton.curm@gmail.com>
2026-04-03 09:32:39 +0300

    Ball class, colors

--- src/03_balls_ro/Assets/_project/CircleVisualTest.cs
+++ src/03_balls_ro/Assets/_project/CircleVisualTest.cs
@@ -1,13 +1,22 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace VisualTests
 {
+    public sealed class Ball
+    {
+        public Vector2 Position;
+        public float Radius;
+        public Color Color;
+    }
+
     public sealed class CircleVisualTest : MonoBehaviour
     {
-        [SerializeField] private Transform _circleTransform = null!;
+        [SerializeField] private Transform _circlePrefab = null!;
 
-        public AnimatedValueConfig Scale = new()
+        [FormerlySerializedAs("Scale")]
+        public AnimatedValueConfig Radius = new()
         {
             ChangeRate = 1,
             Max = 5,
@@ -19,23 +28,74 @@ namespace VisualTests
             Max = 5,
             Min = -10,
         };
+        public float ColorChangeRate = 5;
+
+        [SerializeField] private Color[] _colors = {};
+
+        private Transform[] _circleTransforms = null!;
+        private readonly Ball[] _balls = { new(), new() };
+
+        private void Start()
+        {
+            _circleTransforms = new Transform[_balls.Length];
+            for (int i = 0; i < _balls.Length; i++)
+            {
+                var t = Instantiate(_circlePrefab);
+                _circleTransforms[i] = t;
+            }
+
+            _balls[1].Color = Color.white;
+        }
 
         private void Update()
         {
             {
                 var x = GetAnimatedValue(X);
+                _balls[0].Position.x = x;
 
-                var currentPos = _circleTransform.position;
-                currentPos.x = x;
+                var radius = GetAnimatedValue(Radius);
+                _balls[0].Radius = radius;
 
-                _circleTransform.position = currentPos;
+                var colorIndex = GetAnimatedValue(new()
+                {
+                    ChangeRate = ColorChangeRate,
+                    Min = 0,
+                    Max = _colors.Length,
+                });
+                int baseline = (int) colorIndex;
+                int firstColorIndex = (baseline) % _colors.Length;
+                int secondColorIndex = (baseline + 1) % _colors.Length;
+                float secondColorPortion = colorIndex - baseline;
+                float firstColorPortion = 1 - secondColorPortion;
+                Color a = _colors[firstColorIndex];
+                Color b = _colors[secondColorIndex];
+                Color c = a * firstColorPortion + b * secondColorPortion;
+                _balls[0].Color = c;
             }
 
             {
-                var scale = GetAnimatedValue(Scale);
-                var scaleVector = new Vector3(scale, scale, scale);
-                _circleTransform.localScale = scaleVector;
+                _balls[1].Position.x += 0.01f;
+                _balls[1].Radius += 0.01f;
             }
+
+            for (int i = 0; i < _balls.Length; i++)
+            {
+                Apply(_balls[i], _circleTransforms[i]);
+            }
+        }
+
+        private static void Apply(Ball ball, Transform transform)
+        {
+            Vector3 pos = ball.Position;
+            pos.z = transform.position.z;
+            transform.position = pos;
+
+            var scale = ball.Radius;
+            var scaleVector = new Vector3(scale, scale, scale);
+            transform.localScale = scaleVector;
+
+            var renderer_ = transform.gameObject.GetComponent<SpriteRenderer>();
+            renderer_.color = ball.Color;
         }
 
         private static float GetAnimatedValue(AnimatedValueConfig config)
